4324d4916be37701dbb8f1c8c391488727c2d6de
jubair-jx <jubair.official97@gmail.com>
2023-05-16 12:30:05 +0600

    SomeFeature DOne

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 console.log(process.env.DB_USER);
 
@@ -27,11 +27,34 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const serviceCollection = client.db("carDoctor").collection("carDr");
+    const bookingCollection = client.db("carDoctor").collection("booking");
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: {
+          title: 1,
+          img: 1,
+          service_id: 1,
+          price: 1,
+          description: 1,
+        },
+      };
+      const result = await serviceCollection.findOne(query, options);
+
+      res.send(result);
+    });
+
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
